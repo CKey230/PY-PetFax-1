bffb00f8d8368b876596068d6997261c43a4e408
Ckey230 <Ckey230@gmail.com>
2023-03-26 12:45:47 -0400

    database interaction

--- petfax/fact.py
+++ petfax/fact.py
@@ -1,5 +1,5 @@
 from flask import ( Blueprint, render_template, request, redirect)
-
+from . import models
 
 bp = Blueprint('fact', __name__, url_prefix="/facts")
 
@@ -10,7 +10,15 @@ def new():
 @bp.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        print(request.form)
-        return redirect('/facts')
+        submitter = request.form['submitter']
+        fact = request.form['fact']
 
-    return render_template('facts/index.html')
+        new_fact = models.Fact(submitter=submitter, fact=fact)
+        models.db.session.add(new_fact)
+        models.db.session.commit()
+
+        return redirect('/facts')
+    
+    results = models.Fact.query.all()
+
+    return render_template('facts/index.html', facts=results)
